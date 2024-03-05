b29e4af867f6bdb4ee8c2b66ae920336c783ffa3
ThanMyintMaungThike <thanmyintmaungthike@gmail.com>
2024-03-05 15:40:33 +0630

    sec commit

---
+++ resources/views/articles/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    {{-- @dd($articles) --}}
+    @foreach ($articles as $article)
+    <ul>
+        <li>{{ $article['id'] }}</li>
+        <li>{{ $article['name'] }}</li>
+    </ul>
+
+    @endforeach
+    {{-- <h1>Article One</h1>
+    <h3>Article Two</h3> --}}
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Articles\ArticleController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// static route
+// Route::get('/users', function () {
+//     return "These are the users.";
+// });
+// Dynamic route
+// Route::get('/users/{id}', function ($id) {
+//     return "This is user $id.";
+// });
+
+Route::get('/articles', [ArticleController::class, 'index']);
